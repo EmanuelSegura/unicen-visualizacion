61d766191e0658ba69aadb7885bc9b3841dfd082
Facu Cristobal <facucristobal@gmail.com>
2017-09-01 17:56:07 -0300

    Inicio del juego TP2

--- TP2/ej3p3.js
+++ TP2/ej3p3.js
@@ -1,2 +1,31 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+canvas.onclick = pintarOnClick;
+
+function Circle(paramPosX, paramPosY,paramRadio,paramColor){
+  this.posX = paramPosX;
+  this.posY = paramPosY;
+  this.radio = paramRadio;
+  this.color = paramColor;
+}
+
+function pintarOnClick(e){
+
+    var posX = e.clientX;
+    var posY = e.clientY;
+    var circulo = new Circle(posX,posY,100,"#ffffff");
+
+    var img = new Image();
+    img.src = "images.png";
+
+    img.onload = function(){
+      var image = ctx.drawImage(img,posX-100,posY-100);
+
+      ctx.fillStyle = image;
+      ctx.beginPath();
+      // ctx.arc(circulo.posX,circulo.posY,circulo.radio,0,Math.PI * 2);
+      ctx.fill();
+      ctx.closePath();
+    }
+}
---
+++ TP2/juego.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// function Circle(paramPosX, paramPosY,paramRadio,paramColor){
+//   this.posX = paramPosX;
+//   this.posY = paramPosY;
+//   this.radio = paramRadio;
+//   this.color = paramColor;
+// }
+
+function Tablero (){
+
+}
