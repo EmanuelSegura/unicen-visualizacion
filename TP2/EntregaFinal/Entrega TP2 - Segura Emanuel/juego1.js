9737b55af78bce94b61c6d49fb01215ab4681efb
Emanuel Segura <Emanuel@MacBook-Pro-de-Emanuel.local>
2017-09-14 20:34:25 -0300

    Juego, falta agregar algunas formas

--- TP2/EntregaFinal/Entrega TP2 - Segura Emanuel/juego1.js
+++ TP2/EntregaFinal/Entrega TP2 - Segura Emanuel/juego1.js
@@ -11,121 +11,137 @@ function TableroEscenario (canvas){
   ctx.fillRect(100,20,550,400);
 };
 
-
 //Mini tablero de Fichas para arrastrar
 
 function TableroFichas (canvas){
   let ctx = canvas.getContext("2d");
   ctx.fillStyle="#FFE400";
-  ctx.fillRect(100,450,550,50);
+  ctx.fillRect(100,450,550,300);
 }
-
-
-//Crear Circulo
-class Circulo{
-  constructor(posX,posY,radio,angle,color){
-    this.posX = posX;
-    this.posY = posY;
-    this.radio = radio;
-    this.angle = angle;
-    this.color = color;
-  }
-  draw(){
-    let ctx = canvas.getContext("2d");
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.posX, this.posY, this.radio,this.angle,Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
-  }
-  seleccionar(clix,cliy){
-    let x =  Math.pow((clix-this.posX),2);
-    let y =  Math.pow((cliy-this.posY),2);
-    let d1 = Math.sqrt(x+y);
-    return(d1 < this.radio);
-}
-
-};
 
 //Instancias del Circulo
 let circle1 = new Circulo (150,70,36,0,'#9E52A1');
 let circle2 = new Circulo (300,70,36,0,'#863089');
 let circle3 = new Circulo (450,70,36,0,'#731A76');
 let circle4 = new Circulo (600,70,36,0,'#5D0A60');
-let circle5 = new Circulo (150,370,36,0,'#9E52A1');
-let circle6 = new Circulo (300,370,36,0,'#863089');
-let circle7 = new Circulo (450,370,36,0,'#731A76');
-let circle8 = new Circulo (600,370,36,0,'#5D0A60');
-
-//Circulos se dibujan
-circle1.draw();
-circle2.draw();
-circle3.draw();
-circle4.draw();
-circle5.draw();
-circle6.draw();
-circle7.draw();
-circle8.draw();
-
-//Crear Rectangulo
-class Rectangulo{
-  constructor(posX,posY,width,height,color){
-    this.posX = posX;
-    this.posY = posY;
-    this.width = width;
-    this.height = height;
-    this.color= color;
-  }
-  draw(){
-    let ctx = canvas.getContext("2d");
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.rect(this.posX,this.posY,this.width,this.height);
-    ctx.fill();
-    ctx.closePath();
-  }
-  seleccionar(clix,cliy) {
-  var c=document.getElementById("canvas");
-  var fig = c.getBoundingClientRect();
-  return(this.posX-canvas.offsetLeft < (clix - fig.left)) && (this.lado + this.posX-canvas.offsetLeft > (clix - fig.left)) &&
-  (this.posY-canvas.offsetTop < (cliy- fig.top)) && (this.lado + this.posY-canvas.offsetTop > (cliy- fig.top));
-}
-};
+let circle1F = new Circulo (150,370,36,0,'red');
+let circle2F = new Circulo (300,370,36,0,'red');
+let circle3F = new Circulo (450,370,36,0,'red');
+let circle4F = new Circulo (600,370,36,0,'red');
 
 //Instancias del rectangulo
 let rect1 = new Rectangulo (120,140,64,64,'#9E52A1');
 let rect2 = new Rectangulo (270,140,64,64,'#863089');
 let rect3 = new Rectangulo (420,140,64,64,'#731A76');
 let rect4 = new Rectangulo (570,140,64,64,'#5D0A60');
-let rect5 = new Rectangulo (120,240,64,64,'#9E52A1');
-let rect6 = new Rectangulo (270,240,64,64,'#863089');
-let rect7 = new Rectangulo (420,240,64,64,'#731A76');
-let rect8 = new Rectangulo (570,240,64,64,'#5D0A60');
-
-//Se dibujan los rectangulos
-rect1.draw();
-rect2.draw();
-rect3.draw();
-rect4.draw();
-rect5.draw();
-rect6.draw();
-rect7.draw();
-rect8.draw();
+let rect1F = new Rectangulo (150,420,64,64,'black');
+let rect2F = new Rectangulo (300,420,64,64,'black');
+let rect3F = new Rectangulo (450,420,64,64,'black');
+let rect4F = new Rectangulo (600,420,64,64,'black');
 
 //Arreglo de fichas del tablero
 let arrTablero = new Array();
 
 //Relleno de arreglo
-arrTablero.push(circle1,rect1,circle2,rect2,circle3,rect3,circle4,rect4,circle5,rect5,circle6,rect6,circle7,rect7,circle8,rect8);
+arrTablero.push(circle1,circle2,circle3,circle4,rect1,rect2,rect3,rect4);
+
 
 //Arreglo de fichas para mover
 
 let arrFichas = new Array();
 
 //Relleno del arreglo de fichas
-arrFichas.push(circle1,rect1,circle2,rect2,circle3,rect3,circle4,rect4,circle5,rect5,circle6,rect6,circle7,rect7,circle8,rect8);
+arrFichas.push(circle1F,circle2F,circle3F,circle4F,rect1F,rect2F,rect3F,rect4F);
+// arrFichas.push(circle1F,rect1,circle2F,rect2,circle3F,rect3,circle4F,rect4);
 
 //Arreglo fichas elegidas
 
-let elegido = new Array();
-elegido.push(circle1,rect1,circle2,rect2,circle3,rect3,circle4,rect4,circle5,rect5,circle6,rect6,circle7,rect7,circle8,rect8);
+// let elegido = new Array();
+// elegido.push(circle1,rect1,circle2,rect2,circle3,rect3,circle4,rect4,circle5,rect5,circle6,rect6,circle7,rect7,circle8,rect8);
+
+
+for (let i = 0; i < arrTablero.length; i++) {
+  arrTablero[i].draw();
+}
+
+for (let i = 0; i < arrFichas.length; i++) {
+  arrFichas[i].draw();
+}
+
+let figuraElegida;
+let contadorAcertadas = 0;
+
+canvas.onmousedown=function(event){
+  for (let i = 0; i < arrFichas.length; i++){
+    if(arrFichas[i].click(event.clientX-canvas.offsetLeft,event.clientY-canvas.offsetTop)){ //Compara si (x,y) esta dentro del radio del circulo
+      arrFichas[i].setSeleccion();
+      figuraElegida = arrFichas[i].id; //Para comparar id de ficha elegida con la forma del tablero en mouseup.
+      arrFichas[i].posX = event.clientX-canvas.offsetLeft;
+      arrFichas[i].posY = event.clientY-canvas.offsetTop;
+     break;
+    }
+  }
+
+  canvas.onmousemove = function(event) {
+
+    for (let i = 0; i < arrFichas.length; i++) {
+
+      if(arrFichas[i].seleccionado == true){
+        console.log('if');
+        arrFichas[i].posX = event.clientX-canvas.offsetLeft; //dibuja en todo el x,y y lo va limpiando para arrastrar la figura sola.
+        arrFichas[i].posY = event.clientY-canvas.offsetTop;
+
+        ctx.clearRect(0,0,canvas.width, canvas.height); //limpia continuamente el canvas
+
+        TableroEscenario(canvas); //redibuja 1 vez
+        TableroFichas(canvas); //redibuja 1 vez
+
+        for (let i = 0; i < arrTablero.length ; i++) { //dibuja todas las formas del tablero
+
+          arrTablero[i].draw();
+
+        }
+        for (let j = 0; j < arrFichas.length; j++) { //dibuja todas las fichas
+
+          arrFichas[j].draw();
+
+        }
+
+      }
+    }
+    canvas.onmouseup = function(event) {
+
+
+
+      for (let i = 0; i < arrTablero.length; i++) {
+         if (arrTablero[i].click(event.clientX-canvas.offsetLeft,event.clientY-canvas.offsetTop) && (arrTablero[i].id == figuraElegida)) {
+           console.log(figuraElegida);
+           contadorAcertadas++;
+           if (contadorAcertadas == arrTablero.length){
+             ganaste.innerHTML ="Ganaste!";
+           }
+
+      }
+
+
+      for (let i = 0; i < arrFichas.length; i++) { //Lo vuelve false para poder agarrarlo en caso de ser necesario. (switch)
+        if (arrFichas[i].seleccionado==true) {
+          arrFichas[i].setSeleccion();
+        }
+      }
+
+      canvas.onmousemove = null;
+
+      for (let i = 0; i < arrTablero.length ; i++) {
+
+        arrTablero[i].draw();
+
+      }
+      for (let j = 0; j < arrTablero.length; j++) {
+        arrFichas[j].draw();
+
+      }
+    }
+  }
+}
+};
