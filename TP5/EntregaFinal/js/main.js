007bb2857f958542a42729a09c3a56d0a30450c8
Emanuel Segura <Emanuel@MacBook-Pro-de-Emanuel.local>
2017-11-16 22:15:50 -0300

    Animaciones fix

--- TP5/EntregaFinal/js/main.js
+++ TP5/EntregaFinal/js/main.js
@@ -2,8 +2,7 @@
 
     tweets = [];
 
-    let anim = "scale";
-
+    let anim = "imagesca";
     let cb = new Codebird;
 
     let params = {
@@ -12,9 +11,6 @@
     };
 
     $(document).ready(function(){
-
-        // cb.setProxy("https://cb-proxy.herokuapp.com/");
-
         cb.setConsumerKey("Hq7798E1ZQIXKdK5hq1y6smEl","t8jXiK0998QPP2olkeVojMTFHQjXN0cxuomIzRY5EN8ynSfMYk");
         cb.setToken("925470525463330816-i2d7UrqjNO6CCoRIz3BS5cP9WBT0qTb", "cUNLC5tCQs6NthK7IdZq5OwD198yLOftc84eULf49nPhy");
         console.log( "Cargo correctamente" );
@@ -23,9 +19,9 @@
     });
 
 
-    $(".boton-cargar-tweets").on("click", function(event){ 
+    $(".boton-cargar-tweets").on("click", function(event){
         setSearch(document.getElementById('inputDeBusqueda').value);
-        
+
         cb.__call("search_tweets",params,
 
             function (reply) {
@@ -37,7 +33,7 @@
                     }
                 }
 
-                filterSearch(tweets);
+                filterSearch(tweets,anim);
                 console.log(reply);
                 console.log(tweets);
             }
@@ -49,10 +45,7 @@
         params['q']= inputSearch;
     };
 
-    function filterSearch(reply){
-        
-
-       
+    function filterSearch(reply,anim){
         $(".twitts-box").empty();
         $(".hideImage").remove();
         $(".img-instruccion").css("visibility","hidden");
@@ -61,110 +54,82 @@
         $("#thumbhide").css("visibility","visible");
 
         for (let i=0; i< reply.length;i++){
-            
-            
-            $(".twitts-box").append('<div class="co col-md-4"><a href="#" class="thumbnail"><img id="imagejson" class= "img-responsive img-rounded" src=" '+tweets[i].img+' " alt=""></a></div> <div>'+tweets[i].like+'</div>');  
-            $("#imagejson").addClass("imagerot");
+
+
+            $(".twitts-box").append('<div class="co col-md-4"><a href="#" class="thumbnail"><img id="imagejson" class="img-responsive img-rounded '+anim+'" src=" '+tweets[i].img+' " alt=""></a></div>');
+
         }
+
 
     };
 
-    let i = 0;   
+    $(".btn-scale-anim1").on("click", function(event){
+      clearInterval(tiempo);
+         anim="imagesca";
+          removeClass();
+          filterSearch(tweets,anim);
+        });
+
+    $(".btn-translate-anim2").on("click", function(event){
+      clearInterval(tiempo);
+          anim="imagerot";
+           removeClass();
+           filterSearch(tweets,anim);
+    });
+
+    $(".btn-rotation-anim3").on("click", function(event){
+
+      clearInterval(tiempo);
+        anim="imagetran";
+        removeClass();
+        filterSearch(tweets,anim);
+      });
+
+    function removeClass(){
+      $("#imagejson").removeClass("imagesca imagerot imagetran");
+    }
+
+
+    let i = 0;
 
      $(".galery").on("click", function(e){
         filterSearch(tweets,anim);
        clearInterval(tiempo);
     });
-    $(".carrousel1").on("click", function(e){
 
+
+    $(".carrousel1").on("click", function(e){
+      clearInterval(tiempo);
         carrousel();
     });
 
     function carrousel(){
       console.log(tweets);
+      i=0;
       $(".twitts-box").empty();
-      $(".twitts-box").append('<div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded" src=" '+tweets[i].img+' " alt=""></a></div>');
+      $(".twitts-box").append('<div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded '+anim+'" src=" '+tweets[i].img+' " alt=""></a></div>');
         setIntervaloCarrousel();
     }
 
-
-  // $(document).on("click",".twitts-box",function(e){
-  //     $(".carrousel-izq").on("click",function(){
-  //       correIzq();
-
-  //   });
-  //     $(".carrousel-der").on("click",function(){
-  //       correDer()
-  //   });
-
-
-
-  // });
 
   function correDer(){
     console.log("der");
     if(i<tweets.length-1){
         i++;
         $(".twitts-box").empty();
-        $(".twitts-box").append('<div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded" src=" '+tweets[i].img+' " alt=""></a></div>');
+        $(".twitts-box").append('<div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded '+anim+'" src=" '+tweets[i].img+' " alt=""></a></div>');
     }else{
         i=0;
         $(".twitts-box").empty();
-        $(".twitts-box").append('<div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded" src=" '+tweets[i].img+' " alt=""></a></div>');
+        $(".twitts-box").append('<div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded '+anim+'" src=" '+tweets[i].img+' " alt=""></a></div>');
 
     }
 };
 
-//Carrousel - intervalo
+
 
 let tiempo;
 
 function setIntervaloCarrousel() {
     tiempo = setInterval(correDer, 2000);
 };
-
-
-// function correIzq(){
-//     console.log("izq");
-//     if(i>0){
-//       i--;
-//       $(".twitts-box").empty();
-//       $(".twitts-box").append('<li><a class="glyphicon glyphicon-chevron-left carrousel-izq btn-izq co col-md-6" href="#"></a></li> <div class="co col-md-8 col-md-offset-2"><a href="#" class=""><img id="imagejson" class= "img-responsive img-rounded" src=" '+tweets[i].img+' " alt=""></a></div><li><a class="glyphicon glyphicon-chevron-right carrousel-der btn-der" href="#"></a></li>');
-//   }
-// }
-
-// Animaciones
-
-$(".btn-scale-anim1").on("click", function(event){ 
-     $("#imagejson").removeClass("imagesca imagerot imagetran");
-     $("#imagejson").addClass("imagesca");
-
-     // anim="scale";
-     filterSearch(tweets);
-});
-
-$(".btn-translate-anim2").on("click", function(event){ 
-
-
-     $("#imagejson").removeClass("imagesca imagerot imagetran");
-     $("#imagejson").addClass("imagetran");
-    
-    filterSearch(tweets);
-});
-
-$(".btn-rotation-anim3").on("click", function(event){ 
-     
-     $("#imagejson").removeClass("imagesca imagerot imagetran");
-     $("#imagejson").addClass("imagerot");
-    
-    filterSearch(tweets);
-
-});
-
-
-
-
-
-
-
-
